9d6f56f503a6b70aa3c9f01883a31443cbb50e5c
penglinhan <2453995079@qq.com>
2017-09-26 09:28:47 +0800

    tools progect

---
+++ app/src/main/java/cn/edu/pku/penglinhan/weatherapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.pku.penglinhan.weatherapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * Created by Administrator on 2017/9/25 0025.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
